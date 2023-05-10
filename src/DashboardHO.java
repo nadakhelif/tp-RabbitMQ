8408ac28913f228654190c299bd7f61596798ade
Chaima Bouhlel <chaima.bouhlel@insat.u-carthage.tn>
2023-05-10 12:25:57 +0100

    Add HO dashboard

--- src/DashboardHO.java
+++ src/DashboardHO.java
@@ -1,15 +1,21 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.sql.*;
+import java.awt.*;
+import java.sql.SQLException;
 import java.util.List;
 
-public class TableHo {
+public class DashboardHO {
     final Object[] column = {"Date", "Region", "Product", "Quantity", "Cost", "AMT", "Tax", "Total"};
     private JScrollPane scrollPane;
     private JTable dataTable;
     DefaultTableModel dtm;
-
-    public TableHo() {
+    JLabel titleLabel = new JLabel("Table Data", SwingConstants.CENTER);
+    public DashboardHO(){
+        JFrame frame = new JFrame("HO Dashboard");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Head Office Dashboard");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 18));
         Object[][] data = {};
         this.dtm = new DefaultTableModel(data, this.column);
         this.dataTable = new JTable(dtm);
@@ -20,9 +26,11 @@ public class TableHo {
         } catch (SQLException sqlException) {
 
         }
-
+        frame.setLayout(new BorderLayout());
+        frame.add(titleLabel, BorderLayout.NORTH);
+        frame.add(scrollPane, BorderLayout.CENTER);
+        frame.setVisible(true);
     }
-
     public JScrollPane getScrollPane() {
         return scrollPane;
     }
@@ -31,6 +39,7 @@ public class TableHo {
         dtm.setRowCount(0);
         DataSynchronisationHO dataSynchronisationHO = new DataSynchronisationHO();
         List<Product> productList = dataSynchronisationHO.retrieve();
+        titleLabel.setText("Update done successfully: Added " + productList.size() + "rows to the database.");
         for (Product p : productList) {
             dtm.addRow(new Object[]{p.getDate().toString(),
                     p.getRegion(),
@@ -44,4 +53,5 @@ public class TableHo {
         }
 
     }
+
 }
